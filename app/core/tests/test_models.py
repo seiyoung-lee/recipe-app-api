1388d24b6caf43705d5eb9843f09c3907d9e3e77
Sei-Young Lee <ubcmush99@gmail.com>
2020-10-18 21:49:34 -0300

    added create tag feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,10 @@ from core import models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+def sample_user(email='test@gmail.com', password = 'testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
@@ -36,3 +40,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user = sample_user(),
+            name = 'Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
